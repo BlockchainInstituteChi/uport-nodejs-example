edc272be31ccab506843482f95effeafe15fd6f9
alexander-morris <alex@theblockchaininstitute.org>
2019-05-06 10:47:16 -0500

    clean

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const uportConnect = require('uport-connect');
 const qrcode = require('qrcode-terminal');
 
 const mnidAddress = '2ojEtUXBK2J75eCBazz4tncEWE18oFWrnfJ';
-const signingKey = 'cb89a98b53eec9dc58213e67d04338350e7c15a7f7643468d8081ad2c5ce5480';
+const signingKey  = 'cb89a98b53eec9dc58213e67d04338350e7c15a7f7643468d8081ad2c5ce5480';
 
 const appName = 'The Blockchain Institute';
 
